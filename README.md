a142a5a2afe64d92698458f43265a927f524dd50
abhijeetjuneja <abhijeetjuneja2014@gmail.com>
2020-04-07 03:08:22 +0530

    Data credit and Readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dashboard for Death Penalty in 2019
+
+## Views
+
+  1. Home
+  2. Statewise Data
+  3. Cases Data
+
+## Features
+
+  1. Map Visualization
+  2. Charts
+  3. Filters for Data
+  4. Stats for Data
+
+## Prerequisites
+
+  1. NPM
+
+## Installation
+
+  1. Download or clone the repository.
+  2. Unzip the downloaded folder.
+  3. Through a terminal or Command Prompt all dependencies can be installed using the command "npm install".
+
+## Credit for Data
+
+  [Project 39A](https://www.project39a.com/)
+
+## How to Run the Application
+
+  1. npm start
+  2. Launch http://localhost:3000 in a browser.
+
+
+## OS
+
+  1. Windows 10
+
+## Editor
+  
+  1. VSCode
--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                     <button type="button" class="navbar-toggle header-button collapsed show-side-btn pl-2 pr-2" data-toggle="collapse" data-target="#" aria-expanded="false">
                         <i class="fa fa-bars"></i>
                     </button>
-                    <a class="navbar-brand" href="#">&nbsp;&nbsp;<span class="text-blue">Annual Death Penalty report, 2019</span></a>
+                    <a class="navbar-brand" href="#">&nbsp;&nbsp;<span class="text-blue">Annual Death Penalty report, 2019 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.project39a.com/" target="_blank"  style={{color:'grey', textDecoration:'underline'}} class="pl-4 ml-4">(Data source Credit - Project 39A)</a></span></a>
                 </div>
             </div>
         </nav>
